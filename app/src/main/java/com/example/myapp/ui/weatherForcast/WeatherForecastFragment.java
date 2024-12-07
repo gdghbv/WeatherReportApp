9d70d0ea175b1c9d2858b85f0e5a2c96f421908a
JustWorking <3546124151@qq.com>
2024-12-08 01:51:28 +0800

    创建了底部导航项目

---
+++ app/src/main/java/com/example/myapp/ui/weatherForcast/WeatherForecastFragment.java
@@ -0,0 +1,4 @@
+package com.example.myapp.ui.weatherForcast;
+
+public class WeatherForecastFragment {
+}
---
+++ app/src/main/java/com/example/myapp/ui/weatherForcast/WeatherForecastViewModel.java
@@ -0,0 +1,4 @@
+package com.example.myapp.ui.weatherForcast;
+
+public class WeatherForecastViewModel {
+}
