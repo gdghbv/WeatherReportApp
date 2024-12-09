2fdcbff37e7ff3cf0f561d2c5cca089b67ae6844
JustWorking <3546124151@qq.com>
2024-12-10 05:02:24 +0800

    添加了作品简介

--- app/src/main/java/com/example/myapp/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/myapp/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("1.本应用为天气预报应用，使用了底部导航。应用涉及了网络请求、数据解析、数据展示、线程处理等功能。\n2.使用了NetUtil工具来获取网络接口，在AndroidManifest.xml文件中添加了网络权限");
     }
 
     public LiveData<String> getText() {
